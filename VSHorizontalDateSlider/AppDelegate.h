dd4ce591e7621e046a97d6bab48a22306f7408d1
nvson28 <vnguyen@hoiio.com>
2014-04-13 17:06:27 +0800

    Initial Commit

---
+++ VSHorizontalDateSlider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VSHorizontalDateSlider
+//
+//  Created by Vincent Nguyen on 13/4/14.
+//  Copyright (c) 2014 nvson28. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
