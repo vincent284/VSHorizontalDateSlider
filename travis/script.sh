e94f033d0a9e2519f29fabbdc44e2e21d2d0b2f6
nvson28 <vnguyen@hoiio.com>
2014-04-22 12:36:23 +0800

    Only build for simulator

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-xctool -project VSHorizontalDateSlider.xcodeproj -scheme VSHorizontalDateSlider build
+xctool -project VSHorizontalDateSlider.xcodeproj -scheme VSHorizontalDateSlider -sdk iphonesimulator build
